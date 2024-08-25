ae14122b83bd6647179d6067433899afc5ac8f8d
Reza <i.am.reza.programmer@gmail.com>
2024-08-25 12:06:11 +0330

    start

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,11 @@ document.addEventListener("DOMContentLoaded", (event) => {
           trigger: ".c",
           // toggleActions: "play none none none",  // default // triggerActions: "onEnter onLeave onEnterBack onLeaveBack"
           // toggleActions: "play reverse play reverse",
-            toggleActions: "restart pause resume pause"
+            toggleActions: "restart pause resume pause",
+        //     start and end => start : "top bottom" [trigger position] [scroller position]
+        //     start: "top center",
+            start: "-20px 50%",
+            markers: true, // markers
         },
         x: 500,
         rotate: 360,
