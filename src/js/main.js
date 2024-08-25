62ef3d60442a229892438e530a860313cc24fe92
Reza <i.am.reza.programmer@gmail.com>
2024-08-25 12:20:35 +0330

    scrub

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,20 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger)
 
-    gsap.to('.b' , {
+    gsap.to('.c' , {
         // scrollTrigger: '.c',
         scrollTrigger:{
-          trigger: ".a",
+          trigger: ".c",
           // toggleActions: "play none none none",  // default // triggerActions: "onEnter onLeave onEnterBack onLeaveBack"
           // toggleActions: "play reverse play reverse",
             toggleActions: "restart pause resume pause",
         //     start and end => start : "top bottom" [trigger position] [scroller position]
         //     start: "top center",
-            start: "top 20px",
+            start: "center 80%",
             // end: "bottom center",
-            end: "bottom 80%",
-            endTrigger: '.c',
+            end: "center 10%",
+            // scrub: true, // or 1 2 3 4 => time
+            scrub: 2,
             markers: true, // markers
         },
         x: 500,
