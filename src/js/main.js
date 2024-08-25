ee729c863fbef2813352d42745ffc0ab18b34b69
Reza <i.am.reza.programmer@gmail.com>
2024-08-25 12:30:08 +0330

    pin

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,28 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger)
 
-    let tl = gsap.timeline({
+    gsap.to('.c' , {
+        // scrollTrigger: '.c',
         scrollTrigger:{
-            trigger: ".c",
-            // toggleActions: "play none none none",  // default // triggerActions: "onEnter onLeave onEnterBack onLeaveBack"
-            // toggleActions: "play reverse play reverse",
+          trigger: ".c",
+          // toggleActions: "play none none none",  // default // triggerActions: "onEnter onLeave onEnterBack onLeaveBack"
+          // toggleActions: "play reverse play reverse",
             toggleActions: "restart pause resume pause",
-            //     start and end => start : "top bottom" [trigger position] [scroller position]
-            //     start: "top center",
-            start: "center 80%",
+        //     start and end => start : "top bottom" [trigger position] [scroller position]
+        //     start: "top center",
+            start: "top bottom",
             // end: "bottom center",
-            end: "center 10%",
+            end: "top 100px",
             // scrub: true, // or 1 2 3 4 => time
             scrub: 2,
+            // pin: true,
+            pin: ".b",
             markers: true, // markers
         },
-    })
-
-    tl.to('.c' , {
-        // scrollTrigger: '.c',
-        x: 500,
+        x: 300,
         rotate: 360,
-        duration: 4,
+        ease: "none",
+        duration: 3,
     })
-        .to('.c' , {
-            backgroundColor: "purple",
-            duration: 1,
-        })
-        .to('.c' , {
-            x: 0,
-            duration: 3,
-        })
 })
 
