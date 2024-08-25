321e1ce994126f0e41dfc20c9d1031fdc34bbca3
Reza <i.am.reza.programmer@gmail.com>
2024-08-25 12:50:30 +0330

    scroll sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,20 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger)
+    gsap.defaults({ease: "none", duration: 2})
+    let tl = gsap.timeline();
+    tl.from('.orange' , {xPercent: -100})
+        .from('.blue' , {xPercent: 100})
+        .from(".green" , {yPercent: -100})
+        .from(".pink" , {yPercent: 100})
 
-    gsap.to('.c' , {
-        // scrollTrigger: '.c',
-        scrollTrigger:{
-          trigger: ".c",
-          // toggleActions: "play none none none",  // default // triggerActions: "onEnter onLeave onEnterBack onLeaveBack"
-          // toggleActions: "play reverse play reverse",
-            toggleActions: "restart pause resume pause",
-        //     start and end => start : "top bottom" [trigger position] [scroller position]
-        //     start: "top center",
-            start: "top bottom",
-            // end: "bottom center",
-            end: "top 100px",
-            // scrub: true, // or 1 2 3 4 => time
-            scrub: 2,
-            // pin: true,
-            pin: ".b",
-            markers: true, // markers
-        },
-        x: 300,
-        rotate: 360,
-        ease: "none",
-        duration: 3,
+    ScrollTrigger.create({
+        animation: tl,
+        trigger: document.body,
+        start: 'top top',
+        end: "+=4000",
+        scrub: true,
+        pin: true,
+        anticipatePin: 1,
     })
 })
 
