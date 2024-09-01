455ca0cf6ca90f1a2084a5317101d280d6ced21f
Reza <i.am.reza.programmer@gmail.com>
2024-09-01 17:19:25 +0330

    x scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,16 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger)
-    gsap.defaults({ease: "none", duration: 2})
-    let tl = gsap.timeline();
-    tl.from('.orange' , {xPercent: -100})
-        .from('.blue' , {xPercent: 100})
-        .from(".green" , {yPercent: -100})
-        .from(".pink" , {yPercent: 100})
-
-    ScrollTrigger.create({
-        animation: tl,
-        trigger: document.body,
-        start: 'top top',
-        end: "+=4000",
-        scrub: true,
-        pin: true,
-        anticipatePin: 1,
+    const sections = gsap.utils.toArray('section')
+    gsap.to(sections, {
+        xPercent: -100 * (sections.length - 1),
+        ease: "none",
+        scrollTrigger:{
+            trigger: document.body,
+            pin: true,
+            scrub: 1,
+            snap: 1 / (sections.length - 1),
+            end: () => "+=" + document.body.offsetWidth
+        }
     })
 })
 
