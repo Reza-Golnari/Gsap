f89dde94caf837ba2c43595dc7380a55d729feed
Reza <i.am.reza.programmer@gmail.com>
2024-09-10 11:32:07 +0330

    header

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,5 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollTrigger)
-    const sections = gsap.utils.toArray('section')
-    gsap.to(sections, {
-        xPercent: -100 * (sections.length - 1),
-        ease: "none",
-        scrollTrigger:{
-            trigger: document.body,
-            pin: true,
-            scrub: 1,
-            snap: 1 / (sections.length - 1),
-            end: () => "+=" + document.body.offsetWidth
-        }
-    })
+const typed = new Typed('#typed' , {
+    strings: ['GSAP', 'ScrollTrigger' , 'Animations' , 'TypedJs'],
+    typeSpeed: 100,
+    loop: true,
 })
-
